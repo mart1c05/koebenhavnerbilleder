d9615fd7ada51e4bf4904dcc895351aa923a8d29
Martha Scales <mart1c05@stud.kea.dk>
2022-08-28 13:09:54 +0200

    dark

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 import "./style.css";
+const btnLight = document.querySelector("#light");
+const btnDark = document.querySelector("#dark");
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+function toggleTheme(e) {
+  const mode = e.target.dataset.theme;
+  if (mode !== "dark") {
+    document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+}
+
+btnLight.addEventListener("click", toggleTheme);
+btnDark.addEventListener("click", toggleTheme);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
